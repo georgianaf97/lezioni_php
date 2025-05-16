82782d7a32ce37c8202e188b006ce7ccb23a33d4
Makossa <floreageorgianamaria@gmail.com>
2025-05-16 16:30:24 +0200

    lezione

---
+++ lezione_3/for.php
@@ -0,0 +1,11 @@
+<?php
+
+    for( $i=0; < $_GET['id']; $i++ ) {
+        echo $i;
+        echo '<br>';
+    }   
+
+    // inizializza il ciclo contante parte da zero , il secondo membro è la condizione, essa 
+    // viene verificata ad ogni iterazione, il terzo membro è l'incremento del contatore.
+    // ++ sta per incremento, -- sta per decremento
+    // $i++ è equivalente a $i = $i + 1
